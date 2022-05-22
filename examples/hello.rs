58cd12e1b86b3c32a7a836e3a3a2d37499876ccd
Arthur Melton <amtitan@icloud.com>
2022-05-22 15:10:01 -0400

    example

---
+++ examples/hello.rs
@@ -0,0 +1,25 @@
+use razer::event::event_type::Event::JS;
+use razer::event::handler::EventHandler;
+use razer::listener::Lister;
+use razer::send::send;
+use razer::Value;
+use razer::ws;
+
+#[derive(Copy)]
+pub struct Handler;
+
+impl Clone for Handler {
+    fn clone(&self) -> Self { *self }
+}
+
+impl EventHandler for Handler {
+    fn load(&self, _event: Value, ctx: &ws::Sender) {
+        if let Err(why) = send(ctx, JS, "alert(\"Hello\")") {
+            println!("Error: {}", why);
+        }
+    }
+}
+
+fn main() {
+    Lister::new().start(Handler);
+}
