905912c549e2fbdb08fada029e7c44330f2a3adf
Arthur Melton <amtitan@icloud.com>
2022-05-25 18:36:42 -0400

    save messages

--- examples/chat.rs
+++ examples/chat.rs
@@ -1,9 +1,17 @@
+use std::sync::Mutex;
+
+use lazy_static::lazy_static;
+
 use razer::event::event_type::Event::{EVAL, JS};
 use razer::event::handler::EventHandler;
 use razer::listener::Listener;
 use razer::send::{broadcast, send};
 use razer::Sender;
 use razer::Value;
+
+lazy_static! {
+    static ref MESSAGES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
 
 #[derive(Copy)]
 pub struct Handler;
@@ -15,6 +23,18 @@ impl Clone for Handler {
 }
 
 impl EventHandler for Handler {
+    fn load(&self, _event: Value, ctx: &Sender) {
+        send(
+            ctx,
+            JS,
+            &format!(
+                "document.getElementById(\"text\").innerHTML += \"<p>{}</p>\"",
+                MESSAGES.lock().unwrap().join("</p><p>")
+            ),
+        )
+        .unwrap();
+    }
+
     fn keydown(&self, event: Value, ctx: &Sender) {
         if event["key"].as_str().unwrap() == "Enter" && event["srcElement"]["id"] == "input" {
             send(ctx, EVAL, "document.getElementById(\"input\").value").unwrap();
@@ -23,12 +43,16 @@ impl EventHandler for Handler {
     }
 
     fn eval(&self, event: Value, ctx: &Sender) {
+        MESSAGES
+            .lock()
+            .unwrap()
+            .push(event["event"].as_str().unwrap().to_string());
         broadcast(
             ctx,
             JS,
             &format!(
                 "document.getElementById(\"text\").innerHTML += \"<p>{}</p>\"",
-                event["event"].as_str().unwrap()
+                event["event"].as_str().unwrap().replace("\"", "\\\\\"")
             ),
         );
     }
