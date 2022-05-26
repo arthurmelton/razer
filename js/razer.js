d256357ffd06af7cd304d69ffbd61a6580d47601
Arthur Melton <amtitan@icloud.com>
2022-05-26 09:55:48 -0400

    buffer messages

--- js/razer.js
+++ js/razer.js
@@ -1,33 +1,42 @@
 var events = [];
+var messages = [];
 Object.keys(window).forEach(key => {
     if (/^on/.test(key)) {
         events.push(key.slice(2))
     }
 })
 
-window.addEventListener("load", function(event) {
-    const socket = new WebSocket(location.protocol === 'https:' ? "wss://" : "ws://" + window.location.hostname + ":2794");
-    socket.onopen = function() {
-        socket.send("{\"event_name\":\"" + event.type + "\",\"event\":" + stringify_object(event) + "}");
-        for (var i = 0; i < events.length; i++) {
-            window.addEventListener(events[i], function(event) {
-                socket.send("{\"event_name\":\"" + event.type + "\",\"event\":" + stringify_object(event) + "}");
-            })
-        }
-    }
+const socket = new WebSocket(location.protocol === 'https:' ? "wss://" : "ws://" + window.location.hostname + ":2794");
 
-    socket.onmessage = function(event) {
-        let data = JSON.parse(event.data);
-        if (data.name === "js") {
-            eval(data.data);
-        } else if (data.name === "html") {
-            document.querySelector("body").innerHTML = data.data;
-        } else if (data.name === "eval") {
-            var run = data.data;
-            socket.send("{\"event_name\":\"eval\",\"event\":\"" + eval(run).replaceAll("\"", "\\\"") + "\",\"data\":\"" + run.replaceAll("\"", "\\\"") + "\"}");
+for (var i = 0; i < events.length; i++) {
+    window.addEventListener(events[i], function(event) {
+        var send = "{\"event_name\":\"" + event.type + "\",\"event\":" + stringify_object(event) + "}";
+        try {
+            socket.send("{\"event_name\":\"" + event.type + "\",\"event\":" + stringify_object(event) + "}");
         }
-    };
-})
+        catch(err) {
+            messages.push(send);
+        }
+    })
+}
+
+socket.onopen = function() {
+    for (var i = 0; i < messages.length; i++) {
+        socket.send(messages[i]);
+    }
+}
+
+socket.onmessage = function(event) {
+    let data = JSON.parse(event.data);
+    if (data.name === "js") {
+        eval(data.data);
+    } else if (data.name === "html") {
+        document.querySelector("body").innerHTML = data.data;
+    } else if (data.name === "eval") {
+        var run = data.data;
+        socket.send("{\"event_name\":\"eval\",\"event\":\"" + eval(run).replaceAll("\"", "\\\"") + "\",\"data\":\"" + run.replaceAll("\"", "\\\"") + "\"}");
+    }
+};
 
 function stringify_object(object, depth = 0, max_depth = 2) {
     // change max_depth to see more levels, for a touch event, 2 is good
